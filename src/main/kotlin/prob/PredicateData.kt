776172127d9957c9a2ca20e53605618b6ac45965
Joshua Schmidt <joshua.schmidt@uni-duesseldorf.de>
2019-03-29 20:56:39 +0100

    adapt data class

--- src/main/kotlin/prob/PredicateData.kt
+++ src/main/kotlin/prob/PredicateData.kt
@@ -2,11 +2,11 @@ package prob
 
 data class VariableState(val variableName: String,
                          val variableType: String,
-                         val inputVal: String,
-                         val outputVal: String)
+                         val value: String)
 
 data class GroundTruthComponent(val componentName: String,
                                 val componentAmount: Int)
 
-data class PredicateData(val examples: Set<Set<VariableState>>,
-                         val groundTruth: Set<GroundTruthComponent>)
+data class PredicateData(val positiveInputs: HashSet<Set<VariableState>>,
+                         val negativeInputs: HashSet<Set<VariableState>>,
+                         val groundTruth: HashSet<GroundTruthComponent>)
