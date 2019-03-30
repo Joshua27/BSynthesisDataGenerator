71b9fec36aa1d1f728ca785622a4a84d24cf5888
Joshua Schmidt <joshua.schmidt@uni-duesseldorf.de>
2019-03-30 16:07:17 +0100

    do not install ProB main module twice

--- src/main/kotlin/injector/DataGeneratorModule.kt
+++ src/main/kotlin/injector/DataGeneratorModule.kt
@@ -1,10 +1,9 @@
 package injector
 
 import com.google.inject.AbstractModule
-import de.prob.MainModule
 
 class DataGeneratorModule : AbstractModule() {
     override fun configure() {
-        install(MainModule())
+        //
     }
 }
