b48b2952a71b64fe43fdb8c8976824d21f9367ca
Joshua Schmidt <joshua.schmidt@uni-duesseldorf.de>
2019-03-30 16:06:36 +0100

    add data classes Variable and MetaData, add functions for data class PredicateData

--- src/main/kotlin/datagenerator/PredicateData.kt
+++ src/main/kotlin/datagenerator/PredicateData.kt
@@ -1,8 +1,14 @@
-package prob
+package datagenerator
+
+import java.nio.file.Path
+
+data class Variable(
+    val name: String,
+    val type: String
+)
 
 data class VariableState(
-    val variableName: String,
-    val variableType: String,
+    val variable: Variable,
     val value: String
 )
 
@@ -11,12 +17,21 @@ data class GroundTruthComponent(
     val componentAmount: Int
 )
 
+data class MetaData(val machinePath: String, val machineName: String)
+
+data class RawDataSet(val predicateAst: String, val source: Path)
+
 data class PredicateData(
+    val metaData: MetaData,
     val positiveInputs: HashSet<Set<VariableState>>,
     val negativeInputs: HashSet<Set<VariableState>>,
     val groundTruth: HashSet<GroundTruthComponent>
 ) {
     fun amountOfExamples() = positiveInputs.size + negativeInputs.size
-    fun amountOfVariables() =
-        if (!positiveInputs.isEmpty()) positiveInputs.first().size else negativeInputs.first().size
+
+    fun amountOfVariables() = if (!positiveInputs.isEmpty()) positiveInputs.first().size
+    else negativeInputs.first().size
+
+    fun getVariables() = if (!positiveInputs.isEmpty()) positiveInputs.first().map { it.variable }
+    else negativeInputs.first().map { it.variable }
 }
