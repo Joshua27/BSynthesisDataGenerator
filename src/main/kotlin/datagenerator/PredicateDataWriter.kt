ebec9deb33b5ec34a53cab4db7705fab6d329400
Joshua Schmidt <joshua.schmidt@uni-duesseldorf.de>
2019-05-26 01:02:12 +0200

    adapt output writer for predicates

--- src/main/kotlin/datagenerator/PredicateDataWriter.kt
+++ src/main/kotlin/datagenerator/PredicateDataWriter.kt
@@ -4,7 +4,6 @@ import com.sun.xml.internal.txw2.output.IndentingXMLStreamWriter
 import java.io.File
 import java.io.FileOutputStream
 import java.nio.file.Path
-import java.time.LocalDateTime
 import javax.xml.stream.XMLOutputFactory
 import javax.xml.stream.XMLStreamWriter
 
@@ -13,64 +12,67 @@ fun writePredicateDataSetToFile(predicateDataSet: Set<PredicateData>, target: Pa
         XMLOutputFactory.newFactory().createXMLStreamWriter(FileOutputStream(File(target.toUri())), "UTF-8")
     )
     // xmlWriter.document {
-        //element("synthesis-data") {
-        //    attribute("type", "predicates")
-        //    attribute("created", LocalDateTime.now().toString())
-        predicateDataSet.forEach { predicateData ->
-            xmlWriter.element("record") {
-                attribute("path", predicateData.metaData.machinePath)
-                attribute("machine", predicateData.metaData.machineName)
-                attribute("examples", predicateData.amountOfExamples().toString())
-                attribute("vars", predicateData.amountOfVariables().toString())
-                element("vars") {
-                    predicateData.getVariables().forEach { variable ->
-                        element("var") {
-                            element("name", variable.name)
-                            element("type", variable.type)
-                            element("positive") {
-                                predicateData.positiveInputs.forEachIndexed { i, positiveInput ->
-                                    element(
-                                        "input",
-                                        positiveInput.first { it.variable.name == variable.name }.value
-                                    ) {
-                                        attribute("index", i.toString())
-                                    }
-                                }
-                            }
-                            element("negative") {
-                                predicateData.negativeInputs.forEachIndexed { i, positiveInput ->
-                                    element(
-                                        "input",
-                                        positiveInput.first { it.variable.name == variable.name }.value
-                                    ) {
-                                        attribute("index", i.toString())
-                                    }
+    //element("synthesis-data") {
+    //    attribute("type", "predicates")
+    //    attribute("created", LocalDateTime.now().toString())
+    predicateDataSet.forEach { predicateData ->
+        xmlWriter.element("record") {
+            attribute("path", predicateData.metaData.machinePath)
+            attribute("machine", predicateData.metaData.machineName)
+            attribute("hash", predicateData.metaData.predicateHash.toString())
+            attribute("examples", predicateData.amountOfExamples().toString())
+            attribute("vars", predicateData.amountOfVariables().toString())
+            element("vars") {
+                predicateData.getVariables().forEach { variable ->
+                    element("var") {
+                        element("name", variable.name)
+                        element("type", variable.type)
+                        element("positive") {
+                            predicateData.positiveInputs.forEachIndexed { i, positiveInput ->
+                                element(
+                                    "input",
+                                    positiveInput.first { it.variable.name == variable.name }.value
+                                ) {
+                                    attribute("index", i.toString())
                                 }
                             }
                         }
-                    }
-                }
-                element("ground-truth") {
-                    predicateData.groundTruth.forEach {
-                        element("component") {
-                            element("name", it.componentName)
-                            element("amount", it.componentAmount.toString())
+                        element("negative") {
+                            predicateData.negativeInputs.forEachIndexed { i, positiveInput ->
+                                element(
+                                    "input",
+                                    positiveInput.first { it.variable.name == variable.name }.value
+                                ) {
+                                    attribute("index", i.toString())
+                                }
+                            }
                         }
                     }
                 }
             }
-            //  }
+            element("ground-truth") {
+                predicateData.groundTruth.forEach {
+                    element("component") {
+                        element("name", it.componentName)
+                        element("amount", it.componentAmount.toString())
+                    }
+                }
+            }
         }
+        //  }
+    }
+    println()
+    println("Output written to: $target")
     xmlWriter.flush()
     xmlWriter.close()
 }
-
+/*
 fun XMLStreamWriter.document(init: XMLStreamWriter.() -> Unit): XMLStreamWriter {
     this.writeStartDocument()
     this.init()
     this.writeEndDocument()
     return this
-}
+}*/
 
 fun XMLStreamWriter.element(name: String, init: XMLStreamWriter.() -> Unit): XMLStreamWriter {
     this.writeStartElement(name)
